b63bc57ad70bb04e9ccb49ee624423e7f9373846
Rijan-Stha <shrestharijan056@gmail.com>
2024-03-08 18:56:34 +0545

    create custom error handler

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,28 +1,33 @@
 import User from "../models/user.model.js";
 import bcyrptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({
-            message: "All fields are required"
-        });
-    }
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+      next(errorHandler(400, "All fields are required"));
+  }
 
-    const hashedPassword = bcyrptjs.hashSync(password, 10);
+  const hashedPassword = bcyrptjs.hashSync(password, 10);
 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword
-    });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    try {
-        await newUser.save();
-        res.status(201).json(newUser);
-    } catch (err) {
-        next(err);
-    }
-
-}
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+};
