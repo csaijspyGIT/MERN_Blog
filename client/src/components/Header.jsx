24e35b59095c5c53a2c3dda364a49436d0484376
Rijan-Stha <shrestharijan056@gmail.com>
2024-03-08 16:58:06 +0545

    Use flowbite and react icons to create header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Navbar, TextInput, Button } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+    const path = useLocation().pathname;
   return (
-    <div>Header</div>
-  )
-}
+    <Navbar className="border-b-2">
+      <Link className="self-center font-semibold">
+        <span className="px-2 py-2 bg-gradient-to-r from-slate-600 via-indigo-500 to-purple-600 rounded-lg font-semibold">Rijan's</span>Blog
+        </Link>
+          <TextInput 
+              type="text"
+              placeholder="Search"
+              rightIcon={AiOutlineSearch}
+              className="hidden sm:inline"
+          />
+          <Button className="w-12 h-10 lg:hidden" color="gray">
+              <AiOutlineSearch/>
+          </Button>
+          <div className="flex gap-2 md:order-2">
+              <Button className="w-12 h-10 hidden sm:inline" color="gray">
+                  <FaMoon />
+              </Button>
+              <Link to="/sigin">
+                  <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
+              </Link>
+              <Navbar.Toggle/>
+         </div>
+          <Navbar.Collapse>
+              <Navbar.Link active= {path === "/"} as={'div'}>
+                  <Link to ="/">Home</Link>
+              </Navbar.Link>
+              <Navbar.Link active={path==="/projects"} as={'div'}>
+                  <Link to ="/projects">Projects</Link>
+              </Navbar.Link>
+              <Navbar.Link active={path==="/about"} as={'div'}>
+                  <Link to ="/About">About</Link>
+              </Navbar.Link>
+          </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
