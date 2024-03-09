cf7de8c8aa0ef9172dfec4e3062826d1eb94d97a
Rijan-Stha <shrestharijan056@gmail.com>
2024-03-09 07:24:52 +0545

    Add signup functionality from the frontend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,54 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    
+    if (! formData.username || !formData.password || !formData.email) {
+      return setErrorMessage("Please fill up all the required fields");
+    }
+    
+    try {
+      setIsLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+     
+      if (data.success === false) {
+        return setErrorMessage(data.errMessage);
+      }
+
+      if (res.ok) {
+        navigate('/signin');
+      }
+
+    } catch (err) {
+      setErrorMessage(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex max-w-3xl p-3 mx-auto flex-col md:flex-row gap-6 md:items-center">
@@ -21,34 +68,62 @@ const SignUp = () => {
 
         {/* Right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-5 ">
+          <form className="flex flex-col gap-5 " onSubmit={handleSubmit}>
             <div className="">
               <Label value="Your Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Your Email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="example@gmail.com"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div className="">
               <Label value="Password" />
-              <TextInput type="text" placeholder="Pasword#$23" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Pasword#$23"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
             <Button
               className=" text-white bg-gradient-to-r from-slate-500 via-indigo-500 to-purple-500  w-full font-semibold"
               type="submit"
+              disabled = {isLoading}
             >
-              Sign Up
+              {
+                isLoading ? (
+                  <>
+                    <Spinner size="sm" />
+                    <span className="pl-3"> Loading....</span>
+                  </>
+                ) : (
+                    "Sign Up"
+                )
+              }
             </Button>
           </form>
           <div className="flex flex-row gap-2 mt-3">
             <span>Have an account?</span>
-            <Link to="/signin" className="text-blue-500 font-semibold">Sign in</Link>
+            <Link to="/signin" className="text-blue-500 font-semibold">
+              Sign in
+            </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className="mt-5" color="failure">{errorMessage}</Alert>
+            )
+          }
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -12,13 +12,14 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
-        app.listen(4000, () => {
+        app.listen(PORT, () => {
             console.log("Server is running on port 4000");
         })
     }).catch(err => {
