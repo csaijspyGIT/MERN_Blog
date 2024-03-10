d99cbf7517d7989251d482bb8c6ba54697f51bad
Rijan-Stha <shrestharijan056@gmail.com>
2024-03-11 00:17:02 +0545

    create UI for profile picture upload and progress

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,35 +1,121 @@
-import { TextInput, Button } from "flowbite-react";
-import React from "react";
+import { TextInput, Button, Alert } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [image, setImage] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const filePickerRef = useRef();
 
-  
-    
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
+      setImageURL(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (image) {
+      uploadImage();
+    }
+  }, [image]);
+  const uploadImage = async () => {
+      setImageUploadError(null);
+    const storage = getStorage(app);
+    const fileName = Date.now() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageUploadError("File must be less than 2MB");
+          setImageUploadProgress(null);
+          setImage(null);
+          setImageURL(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImage(downloadURL);
+        });
+      }
+    );
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="text-center text-3xl my-7 font-semibold">Profile</h1>
-      <form className="flex flex-col gap-5">
-        <div className="w-32 h-32 self-center shadow-md rounded-full">
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          ref={filePickerRef}
+          onChange={handleImageChange}
+          hidden
+        />
+        <div
+          onClick={() => filePickerRef.current.click()}
+          className="relative w-32 h-32 self-center shadow-md rounded-full overflow-hidden"
+        >
+          {imageUploadProgress && (
+            <CircularProgressbar
+              value={imageUploadProgress || 0}
+              text={`${imageUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62,152,199, ${imageUploadProgress / 100})`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageURL || currentUser.profilePicture}
             alt="ProfilePicture"
-            className="rounded-full w-full h-full border-8 border-[lightgray] object-cover"
+            className={`rounded-full w-full h-full border-8 border-[lightgray] object-cover  ${imageUploadProgress && imageUploadProgress < 100 && 'opacity-60'}`}
           />
         </div>
-        <div className="flex flex-col gap-3">
-          <TextInput type="text" id="username" placeholder="username" defaultValue={currentUser.username} />
-          <TextInput type="email" id="email" defaultValue={currentUser.email} />
-          <TextInput type="password" id="password" className="" />
-          <Button
-            type="submit"
-            className="bg-gradient-to-r from-slate-500 via-indigo-500 to-violet-500"
-            outline
-          >
-            Update
-          </Button>
-        </div>
+        {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput type="email" id="email" defaultValue={currentUser.email} />
+        <TextInput type="password" id="password" className="" />
+        <Button
+          type="submit"
+          className="bg-gradient-to-r from-slate-500 via-indigo-500 to-violet-500"
+          outline
+        >
+          Update
+        </Button>
+
         <div className="flex justify-between items-center text-red-600 font-semibold cursor-pointer">
           <span>Delete Account</span>
           <span>Sign Out</span>
